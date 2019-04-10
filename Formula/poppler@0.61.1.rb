b4a126cb1b6bafb38a404ad8613318add4910a22
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2019-04-10 20:27:11 +0700

    Add formula for old poppler-0.61.1.

---
+++ Formula/poppler@0.61.1.rb
@@ -0,0 +1,88 @@
+# poppler-0.61.1
+# source: https://raw.githubusercontent.com/Homebrew/homebrew-core/f1a91173c2b77a804c5c31ba6d60118840406ff6/Formula/poppler.rb
+class PopplerAT0611 < Formula
+  desc "PDF rendering library (based on the xpdf-3.0 code base)"
+  homepage "https://poppler.freedesktop.org/"
+  url "https://poppler.freedesktop.org/poppler-0.61.1.tar.xz"
+  sha256 "1266096343f5163c1a585124e9a6d44474e1345de5cdfe55dc7b47357bcfcda9"
+
+  bottle do
+    sha256 "19c1f79d20eb82931cf288cd66da19972537f3bb6862d31f05ee13fba69ad295" => :high_sierra
+    sha256 "a81f8e5c998238f2c399c4917eff6b14ef1fa708841ff5b21e0a868391071e4b" => :sierra
+    sha256 "1621eced6c3d6d11a1e04df49e0d1619a465f8922a47f307e21855ceb1e77159" => :el_capitan
+  end
+
+  option "with-qt", "Build Qt5 backend"
+  option "with-little-cms2", "Use color management system"
+
+  deprecated_option "with-qt4" => "with-qt"
+  deprecated_option "with-qt5" => "with-qt"
+  deprecated_option "with-lcms2" => "with-little-cms2"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "gobject-introspection"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "openjpeg"
+  depends_on "qt" => :optional
+  depends_on "little-cms2" => :optional
+
+  conflicts_with "pdftohtml", "pdf2image", "xpdf",
+    :because => "poppler, pdftohtml, pdf2image, and xpdf install conflicting executables"
+
+  resource "font-data" do
+    url "https://poppler.freedesktop.org/poppler-data-0.4.8.tar.gz"
+    sha256 "1096a18161f263cccdc6d8a2eb5548c41ff8fcf9a3609243f1b6296abdf72872"
+  end
+
+  needs :cxx11 if build.with?("qt") || MacOS.version < :mavericks
+
+  def install
+    ENV.cxx11 if build.with?("qt") || MacOS.version < :mavericks
+
+    args = std_cmake_args + %w[
+      -DENABLE_XPDF_HEADERS=ON
+      -DENABLE_GLIB=ON
+      -DBUILD_GTK_TESTS=OFF
+      -DWITH_GObjectIntrospection=ON
+      -DENABLE_QT4=OFF
+    ]
+
+    if build.with? "qt"
+      args << "-DENABLE_QT5=ON"
+    else
+      args << "-DENABLE_QT5=OFF"
+    end
+
+    if build.with? "little-cms2"
+      args << "-DENABLE_CMS=lcms2"
+    else
+      args << "-DENABLE_CMS=OFF"
+    end
+
+    system "cmake", ".", *args
+    system "make", "install"
+    resource("font-data").stage do
+      system "make", "install", "prefix=#{prefix}"
+    end
+
+    libpoppler = (lib/"libpoppler.dylib").readlink
+    ["#{lib}/libpoppler-cpp.dylib", "#{lib}/libpoppler-glib.dylib",
+     *Dir["#{bin}/*"]].each do |f|
+      macho = MachO.open(f)
+      macho.change_dylib("@rpath/#{libpoppler}", "#{lib}/#{libpoppler}")
+      macho.write!
+    end
+  end
+
+  test do
+    system "#{bin}/pdfinfo", test_fixtures("test.pdf")
+  end
+end
